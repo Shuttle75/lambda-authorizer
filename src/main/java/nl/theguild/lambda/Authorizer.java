1d41c3fbea0de3a490508aae90d83c228c12e591
Oleksii Osadchyi <oleksii_osadchyi@epam.com>
2021-09-05 04:15:17 +0300

    Add Auth0 parser

--- src/main/java/nl/theguild/lambda/Authorizer.java
+++ src/main/java/nl/theguild/lambda/Authorizer.java
@@ -1,21 +1,10 @@
-/*
- * Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except
- * in compliance with the License. A copy of the License is located at
- *
- * http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
-
 package nl.theguild.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import nl.theguild.lambda.model.AuthorizerResponse;
 import nl.theguild.lambda.model.aws.PolicyDocument;
 import nl.theguild.lambda.model.aws.Statement;
@@ -40,6 +29,14 @@ public class Authorizer implements RequestHandler<APIGatewayProxyRequestEvent, A
 
         Map<String, String> ctx = new HashMap<>();
         ctx.put("username", JwtUtils.extractUserName(jwt));
+
+        System.out.println("jwt " + jwt);
+
+        DecodedJWT decodedJWT = JWT.decode(jwt);
+
+        System.out.println(" iss " + decodedJWT.getIssuer());
+        System.out.println(" sub " + decodedJWT.getSubject());
+        System.out.println(" aud " + decodedJWT.getAudience());
 
         APIGatewayProxyRequestEvent.ProxyRequestContext proxyContext = request.getRequestContext();
         APIGatewayProxyRequestEvent.RequestIdentity identity = proxyContext.getIdentity();
